a49863cf3c11a9c669174d36328df1ec0f04c7c5
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-11-07 11:30:28 +0100

    Initialize Spring Boot project

---
+++ src/main/java/com/krzysztofpapiernik/interview/elevators/ElevatorsApplication.java
@@ -0,0 +1,13 @@
+package com.krzysztofpapiernik.interview.elevators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElevatorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krzysztofpapiernik/interview/elevators/ElevatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krzysztofpapiernik.interview.elevators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
