226e99a46b8857a71bfaaf8c40676d5da8efcfb1
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-11-08 16:54:33 +0100

    Add Response Dto to be able to send back more generic response messages

---
+++ src/main/java/com/krzysztofpapiernik/interview/elevators/controller/dto/ResponseDto.java
@@ -0,0 +1,14 @@
+package com.krzysztofpapiernik.interview.elevators.controller.dto;
+
+import java.util.Map;
+
+public record ResponseDto<T>(T data, Map<String, String> errors) {
+
+    public ResponseDto(T data){
+        this(data, null);
+    }
+
+    public ResponseDto(Map<String, String> errors){
+        this(null, errors);
+    }
+}
