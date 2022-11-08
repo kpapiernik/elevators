41ebd759a3ed0011ff48f97d1877f488ecb1e875
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-11-08 16:42:56 +0100

    Add Validation Exception

---
+++ src/main/java/com/krzysztofpapiernik/interview/elevators/exception/ValidationException.java
@@ -0,0 +1,19 @@
+package com.krzysztofpapiernik.interview.elevators.exception;
+
+import java.util.Map;
+
+public class ValidationException extends RuntimeException {
+
+    private Map<String, String> errors;
+    public ValidationException(Map<String, String> errors) {
+        this.errors = errors;
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public Map<String, String> getErrors(){
+        return errors;
+    }
+}
