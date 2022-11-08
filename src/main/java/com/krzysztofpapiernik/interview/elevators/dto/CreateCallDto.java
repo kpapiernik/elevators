e9253e459273574b9bc4279589c73e78456d86ee
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-11-08 17:21:55 +0100

    Add JSON attributes validation in CreateCall Dto

--- src/main/java/com/krzysztofpapiernik/interview/elevators/dto/CreateCallDto.java
+++ src/main/java/com/krzysztofpapiernik/interview/elevators/dto/CreateCallDto.java
@@ -1,4 +1,24 @@
 package com.krzysztofpapiernik.interview.elevators.dto;
 
+import com.krzysztofpapiernik.interview.elevators.exception.ValidationException;
+
+import java.util.HashMap;
+
 public record CreateCallDto(Long id, Integer floor) {
+
+    public CreateCallDto{
+        var errors = new HashMap<String, String>();
+
+        if(id == null){
+            errors.put("id", "is null");
+        }
+
+        if(floor == null){
+            errors.put("floor", "is null");
+        }
+
+        if(!errors.isEmpty()){
+            throw new ValidationException(errors);
+        }
+    }
 }
