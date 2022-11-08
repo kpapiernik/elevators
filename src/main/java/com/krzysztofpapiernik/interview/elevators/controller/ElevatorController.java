a4a43c35bbf35809a6cd8c8c57cdd7110c6a3bc7
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-11-08 17:22:23 +0100

    Add Elevator Controller to provide REST API

---
+++ src/main/java/com/krzysztofpapiernik/interview/elevators/controller/ElevatorController.java
@@ -0,0 +1,32 @@
+package com.krzysztofpapiernik.interview.elevators.controller;
+
+
+import com.krzysztofpapiernik.interview.elevators.controller.dto.ResponseDto;
+import com.krzysztofpapiernik.interview.elevators.dto.GetElevatorDto;
+import com.krzysztofpapiernik.interview.elevators.service.ElevatorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/elevators")
+@RequiredArgsConstructor
+public class ElevatorController {
+
+    private final ElevatorService elevatorService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseDto<GetElevatorDto> addElevator(){
+        return new ResponseDto<>(elevatorService.addElevator());
+    }
+
+    @GetMapping
+    public ResponseDto<List<GetElevatorDto>> getAllElevators(){
+        return new ResponseDto<>(elevatorService.getAllElevators());
+    }
+
+    public
+}
