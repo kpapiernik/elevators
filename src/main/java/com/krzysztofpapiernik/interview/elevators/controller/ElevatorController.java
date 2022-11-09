4553b8639806a0969aad4d5f57bd983a106dad5e
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-11-09 17:03:11 +0100

    Add the rest of the API methods: - get specific elevator by providing its id - delete specific elevator - add elevator call on specific floor - make next step of the elevators simulation

--- src/main/java/com/krzysztofpapiernik/interview/elevators/controller/ElevatorController.java
+++ src/main/java/com/krzysztofpapiernik/interview/elevators/controller/ElevatorController.java
@@ -2,6 +2,8 @@ package com.krzysztofpapiernik.interview.elevators.controller;
 
 
 import com.krzysztofpapiernik.interview.elevators.controller.dto.ResponseDto;
+import com.krzysztofpapiernik.interview.elevators.dto.CreateCallDto;
+import com.krzysztofpapiernik.interview.elevators.dto.GetElevatorCallsDto;
 import com.krzysztofpapiernik.interview.elevators.dto.GetElevatorDto;
 import com.krzysztofpapiernik.interview.elevators.service.ElevatorService;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,26 @@ public class ElevatorController {
     @GetMapping
     public ResponseDto<List<GetElevatorDto>> getAllElevators(){
         return new ResponseDto<>(elevatorService.getAllElevators());
+    }
+
+    @GetMapping("/{elevatorId}")
+    public ResponseDto<GetElevatorDto> getElevator(@PathVariable("elevatorId") Long id){
+        return new ResponseDto<>(elevatorService.getElevator(id));
+    }
+
+    @DeleteMapping("/{elevatorId}")
+    public void deleteElevator(@PathVariable("elevatorId") Long id){
+        elevatorService.deleteElevator(id);
+    }
+
+    @PostMapping("/calls")
+    public ResponseDto<GetElevatorCallsDto> addCall(@RequestBody CreateCallDto dto){
+        return new ResponseDto<>(elevatorService.makeCall(dto));
+    }
+
+    @PostMapping("/move")
+    public ResponseDto<List<GetElevatorDto>> moveElevators(){
+        return new ResponseDto<>(elevatorService.moveAllElevators());
     }
 
 }
